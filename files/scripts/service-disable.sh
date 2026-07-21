59c59363266443d7ac7bf5fc480fe45c59e14d93
Fyllus <fylluszophia@proton.me>
2026-07-21 13:24:05 -0300

    chore: update cleanup cicle to a clean way to ensure each config will run

--- files/scripts/service-disable.sh
+++ files/scripts/service-disable.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-systemctl disable ModemManager.service sshd.service thermald.service geoclue.service iio-sensor-proxy.service switcheroo-control.service bolt.service || true
+
+systemctl disable ModemManager.service || true
+systemctl disable sshd.service || true
+systemctl disable thermald.service || true
+systemctl disable geoclue.service || true
+systemctl disable iio-sensor-proxy.service || true
+systemctl disable switcheroo-control.service || true
+systemctl disable bolt.service || true
 
 rm -f /etc/gnome-initial-setup-done
 rm -f /var/home/*/.config/gnome-initial-setup-done
